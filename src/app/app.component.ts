11486eb572beda1dd11516d5820222475fa2a54e
metaheather <heathernetheridge@gmail.com>
2021-10-17 22:41:46 -0500

    adds logic for looping through posts array and displaying pposts in the app component template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards';
+
+  posts = [
+    {
+      title: 'Neat Tree',
+      imgUrl: 'assets/tree.jpeg',
+      imgAlt: 'tree on a hill in front of a clear blue sky',
+      username: 'nature',
+      content: 'I saw this neat tree today'
+    },
+    {
+      title: 'snowy Mountain',
+      imgUrl: 'assets/mountain.jpeg',
+      imgAlt: '',
+      username: 'mountainlover',
+      content: 'Here is a picture of a snowy mountain'
+    },
+    {
+      title: 'Mountain Biking',
+      imgUrl: 'assets/biking.jpeg',
+      imgAlt: '',
+      username: 'biking1222',
+      content: 'I did some biking today'
+    }
+  ]
 }
